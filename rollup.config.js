2e3afa5dceff0a6150c4482cb8ce783fdcf0c4aa
Bogdan Chadkin <trysound@yandex.ru>
2018-08-22 02:29:33 +0300

    Provide "global" shim for each bundle
    I'm currently bundle my production app with rollup and found unexpected
    `global` stuff in consumed modules. I think this should be handled in
    library side and provide browser friendly bundle. So the only
    requirement could be shimming/replacing `NODE_ENV`.

--- rollup.config.js
+++ rollup.config.js
@@ -109,6 +109,7 @@ export default [
     plugins: [
       createFlowBundlePlugin,
       babel(getBabelOptions()),
+      nodeGlobals(),
       replace({'process.env.VERSION': JSON.stringify(pkg.version)}),
       sizeSnapshot(snapshotOptions)
     ]
@@ -120,6 +121,7 @@ export default [
     external,
     plugins: [
       babel(getBabelOptions()),
+      nodeGlobals(),
       replace({'process.env.VERSION': JSON.stringify(pkg.version)}),
       sizeSnapshot(snapshotOptions)
     ]
